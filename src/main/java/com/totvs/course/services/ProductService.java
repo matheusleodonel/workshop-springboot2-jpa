349ddd1e1e3d7ed1faacf5005b863c8fb2cea9d6
Matheus Leodonel <mleodonel@gmail.com>
2021-12-14 16:11:58 -0300

    Add Product and Many-to-Many association with JoinTable

---
+++ src/main/java/com/totvs/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.totvs.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.totvs.course.entities.Product;
+import com.totvs.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired // insert dependencies
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
